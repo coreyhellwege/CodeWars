72528490ec490357ee11687832eb32c633bd701d
Corey Hellwege <coreyhellwege@gmail.com>
2022-05-12 21:10:31 +1000

    String repeat challenge

---
+++ repeat.js
@@ -0,0 +1,7 @@
+/* 
+    Write a function called repeatStr which repeats a given string exactly n times.
+    EG: repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+*/
+
+const repeatStr = (interval, str) => str.repeat(interval)
+console.log(repeatStr(5, 'Hi Wes '))
